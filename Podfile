3f9f82e13ee5eb113779c7ab4925ddb5d2803ca5
liuzhiyi1992 <55137840@qq.com>
2016-09-13 15:41:23 +0800

    1.gitignore 2.podFile 3.update code

---
+++ Podfile
@@ -0,0 +1,6 @@
+target 'UCToutiaoClone' do
+    pod 'AFNetworking', '3.1.0'
+    pod 'UITableView+FDTemplateLayoutCell','1.4'
+    pod 'SVProgressHUD'
+    pod 'MJRefresh'
+end
--- UCToutiaoClone/Controller/MainHomeController.m
+++ UCToutiaoClone/Controller/MainHomeController.m
@@ -10,4 +10,37 @@
 
 @implementation MainHomeController
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.view setBackgroundColor:[UIColor grayColor]];
+    [self setupCustomNavView];
+    [self setupCategoryBar];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self.navigationController setNavigationBarHidden:YES animated:YES];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    [self.navigationController setNavigationBarHidden:NO animated:YES];
+}
+
+- (void)setupCustomNavView {
+    
+}
+
+- (void)setupCategoryBar {
+    
+}
+
+- (void)queryCategoryData {
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
 @end
