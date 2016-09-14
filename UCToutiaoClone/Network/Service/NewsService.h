6e8e9cdc4e9564282a517657406342e29d006b35
liuzhiyi1992 <55137840@qq.com>
2016-09-14 22:24:18 +0800

    NewsService

---
+++ UCToutiaoClone/Network/Service/NewsService.h
@@ -0,0 +1,13 @@
+//
+//  NewsService.h
+//  UCToutiaoClone
+//
+//  Created by lzy on 16/9/14.
+//  Copyright © 2016年 lzy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NewsService : NSObject
+
+@end
---
+++ UCToutiaoClone/Network/Service/NewsService.m
@@ -0,0 +1,13 @@
+//
+//  NewsService.m
+//  UCToutiaoClone
+//
+//  Created by lzy on 16/9/14.
+//  Copyright © 2016年 lzy. All rights reserved.
+//
+
+#import "NewsService.h"
+
+@implementation NewsService
+
+@end
