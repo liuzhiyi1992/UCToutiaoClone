8511bf75e6a0d8bcf72dc5e1b286720019b79225
liuzhiyi1992 <55137840@qq.com>
2016-09-15 00:07:40 +0800

    接口默认参数

--- UCToutiaoClone/Network/UCTNetworkManager.m
+++ UCToutiaoClone/Network/UCTNetworkManager.m
@@ -13,9 +13,18 @@
 @end
 
 @implementation UCTNetworkManager
+//stats params
 + (NSDictionary *)addDefaultParameters:(NSDictionary *)undressedParam {
     NSMutableDictionary *mutDict = [NSMutableDictionary dictionaryWithDictionary:undressedParam];
-    //stats params
+    [mutDict setValue:@"1.5.0.805" forKey:@"ve"];
+    [mutDict setValue:@"iphone" forKey:@"fe"];
+    [mutDict setValue:@"iPhone7,1" forKey:@"mi"];
+    [mutDict setValue:@"195" forKey:@"pf"];
+    [mutDict setValue:@"isp:电信;prov:广东;city:广州;na:中国;cc:CN;ac:cp=" forKey:@"cp"];
+    [mutDict setValue:@"bTkwBTigl5NEMRtjFm+RjbHqHnskMiIuAfMEejMnsF3quRENtz7M+1hVGmkdEw==" forKey:@"ei"];
+//    pf=195
+//    cp=isp:电信;prov:广东;city:广州;na:中国;cc:CN;ac:cp=
+//    ei=bTkwBTigl5NEMRtjFm+RjbHqHnskMiIuAfMEejMnsF3quRENtz7M+1hVGmkdEw==
     return mutDict;
 }
 
