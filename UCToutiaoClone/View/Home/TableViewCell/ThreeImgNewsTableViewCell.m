23108d2986729f6ed1015ab5374cb7f9da7bdb3c
liuzhiyi1992 <55137840@qq.com>
2016-09-20 12:02:48 +0800

    threeImgNewsCell 约束

--- UCToutiaoClone/View/Home/TableViewCell/ThreeImgNewsTableViewCell.m
+++ UCToutiaoClone/View/Home/TableViewCell/ThreeImgNewsTableViewCell.m
@@ -10,6 +10,15 @@
 #import "ZYHArticleModel.h"
 #import "Masonry.h"
 
+#define TOP_BOTTOM_MARGIN_TITLE 10
+#define BOTTOM_MARGIN_CELL 10
+#define LEADING_IMAGE_VIEW 5
+#define LEADING_TITLE_LABEL 8
+#define GAP_IMAGE_VIEW 1
+
+#define FONT_SIZE_TITLE 12.f
+#define FONT_SIZE_SOURCE_LABEL 8.f
+
 //todo 组装
 @interface ThreeImgNewsTableViewCell ()
 @property (strong, nonatomic) UILabel *titleLabel;
@@ -34,20 +43,53 @@
 }
 
 - (void)setupCell {
+    self.titleLabel = [[UILabel alloc] init];
+    [_titleLabel setFont:[UIFont systemFontOfSize:FONT_SIZE_TITLE]];
+    [self.contentView addSubview:_titleLabel];
+    
+    self.sourceLabel = [[UILabel alloc] init];
+    [_sourceLabel setFont:[UIFont systemFontOfSize:FONT_SIZE_SOURCE_LABEL]];
+    [self.contentView addSubview:_sourceLabel];
+    
     self.leftImageView = [[UIImageView alloc] init];
     self.centerImageView = [[UIImageView alloc] init];
     self.rightImageView = [[UIImageView alloc] init];
+    [self.contentView addSubview:_leftImageView];
+    [self.contentView addSubview:_centerImageView];
+    [self.contentView addSubview:_rightImageView];
     
-    [_imageViewList[0] mas_makeConstraints:^(MASConstraintMaker *make) {
-        
+    [_titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.leading.equalTo(self.contentView).offset(LEADING_TITLE_LABEL);
+        make.top.equalTo(self.contentView).offset(TOP_BOTTOM_MARGIN_TITLE);
     }];
     
-    [_imageViewList[1] mas_makeConstraints:^(MASConstraintMaker *make) {
-        
+    [_leftImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.width.equalTo(_leftImageView.mas_height);
+        make.top.equalTo(_titleLabel.mas_bottom).offset(TOP_BOTTOM_MARGIN_TITLE);
+        make.leading.equalTo(self.contentView).offset(LEADING_IMAGE_VIEW);
+        make.trailing.equalTo(_centerImageView).offset(GAP_IMAGE_VIEW);
     }];
     
-    [_imageViewList[2] mas_makeConstraints:^(MASConstraintMaker *make) {
-        
+    [_centerImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.width.equalTo(_leftImageView);
+        make.height.equalTo(_leftImageView);
+        make.top.equalTo(_leftImageView);
+//        make.bottom.equalTo(_leftImageView);
+        make.trailing.equalTo(_rightImageView).offset(GAP_IMAGE_VIEW);
+    }];
+    
+    [_rightImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.width.equalTo(_leftImageView);
+        make.height.equalTo(_leftImageView);
+        make.top.equalTo(_leftImageView);
+//        make.bottom.equalTo(_leftImageView);
+        make.trailing.equalTo(self.contentView).offset(LEADING_IMAGE_VIEW);
+    }];
+    
+    [_sourceLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.equalTo(self.contentView).offset(-BOTTOM_MARGIN_CELL);
+        make.top.equalTo(_leftImageView).offset(8);
+        make.leading.equalTo(_titleLabel);
     }];
     
     
