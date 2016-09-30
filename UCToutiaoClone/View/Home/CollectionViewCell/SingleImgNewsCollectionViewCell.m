1e9fc6f2ac3cb9a01cf8aa78eadf446661f89bba
liuzhiyi1992 <55137840@qq.com>
2016-09-30 14:34:57 +0800

    update

--- UCToutiaoClone/View/Home/CollectionViewCell/SingleImgNewsCollectionViewCell.m
+++ UCToutiaoClone/View/Home/CollectionViewCell/SingleImgNewsCollectionViewCell.m
@@ -131,11 +131,4 @@
     }
 }
 
-
-//- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-//    [super setSelected:selected animated:animated];
-//
-//    // Configure the view for the selected state
-//}
-
 @end
