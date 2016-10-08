a2dbab72976b6bd07d25415983b35f6452d84003
liuzhiyi1992 <55137840@qq.com>
2016-10-08 14:39:52 +0800

    tabbarController

---
+++ UCToutiaoClone/Controller/TabBar/UCTTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  UCTTabBarController.h
+//  UCToutiaoClone
+//
+//  Created by lzy on 16/10/8.
+//  Copyright © 2016年 lzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UCTTabBarController : UITabBarController
+
+@end
---
+++ UCToutiaoClone/Controller/TabBar/UCTTabBarController.m
@@ -0,0 +1,28 @@
+//
+//  UCTTabBarController.m
+//  UCToutiaoClone
+//
+//  Created by lzy on 16/10/8.
+//  Copyright © 2016年 lzy. All rights reserved.
+//
+
+#import "UCTTabBarController.h"
+
+@interface UCTTabBarController ()
+
+@end
+
+@implementation UCTTabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
