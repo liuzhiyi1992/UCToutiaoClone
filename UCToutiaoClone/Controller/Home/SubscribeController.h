73deed0844df1399b694f1f406e025c06e03b917
liuzhiyi1992 <55137840@qq.com>
2016-10-09 22:57:56 +0800

    SubscribeController

---
+++ UCToutiaoClone/Controller/Home/SubscribeController.h
@@ -0,0 +1,13 @@
+//
+//  SubscribeController.h
+//  UCToutiaoClone
+//
+//  Created by lzy on 16/10/9.
+//  Copyright © 2016年 lzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SubscribeController : UIViewController
+
+@end
