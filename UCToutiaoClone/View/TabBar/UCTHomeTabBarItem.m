4ee0c75e3e0158ea3632c7a081fb0488b4278a47
liuzhiyi1992 <55137840@qq.com>
2016-10-11 15:19:39 +0800

    晚上homeTabBarItem 在两种status下的动画切换

--- UCToutiaoClone/View/TabBar/UCTHomeTabBarItem.m
+++ UCToutiaoClone/View/TabBar/UCTHomeTabBarItem.m
@@ -56,109 +56,113 @@
 }
 
 - (void)releaseAnim {
-    //spotImageView
-    _spotImageView.alpha = 1;
-    CABasicAnimation *spotImageViewScale = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
-    spotImageViewScale.fromValue = @(1);
-    spotImageViewScale.toValue = @(0);
-    spotImageViewScale.duration = 0.3f;
-    [_spotImageView.layer addAnimation:spotImageViewScale forKey:@"soptImageView"];
+    if (_itemStatus == HomeTabBarItemStatusWeather) {
+        //spotImageView
+        _spotImageView.alpha = 1;
+        CABasicAnimation *spotImageViewScale = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+        spotImageViewScale.fromValue = @(1);
+        spotImageViewScale.toValue = @(0);
+        spotImageViewScale.duration = 0.3f;
+        [_spotImageView.layer addAnimation:spotImageViewScale forKey:@"soptImageView"];
+        
+        [CATransaction begin];
+        [CATransaction setValue:(id)kCFBooleanTrue forKey:kCATransactionDisableActions];
+        _spotImageView.layer.transform = CATransform3DMakeScale(0, 0, 1);
+        [CATransaction commit];
+    } else if (_itemStatus == HomeTabBarItemStatusReading) {
+        _spotImageView.alpha = 0;
+        [UIView animateWithDuration:0.3f animations:^{
+            self.mainImageView.alpha = 1;
+        } completion:^(BOOL finished) {
+            [self.titleLabel setText:@"首页"];
+        }];
+        //mainImageView
+        CABasicAnimation *mainImageViewScaleAnim = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+        mainImageViewScaleAnim.fromValue = @(1.2);
+        mainImageViewScaleAnim.toValue = @(1);
+        mainImageViewScaleAnim.duration = 0.3f;
+        [self.mainImageView.layer addAnimation:mainImageViewScaleAnim forKey:@"mainImageView"];
     
-    [CATransaction begin];
-    [CATransaction setValue:(id)kCFBooleanTrue forKey:kCATransactionDisableActions];
-    _spotImageView.layer.transform = CATransform3DMakeScale(0, 0, 1);
-    [CATransaction commit];
-//    _spotImageView.alpha = 0;
-//    [UIView animateWithDuration:0.3f animations:^{
-//        self.mainImageView.alpha = 1;
-//    } completion:^(BOOL finished) {
-//        [self.titleLabel setText:@"首页"];
-//    }];
-//    //mainImageView
-//    CABasicAnimation *mainImageViewScaleAnim = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
-//    mainImageViewScaleAnim.fromValue = @(1.2);
-//    mainImageViewScaleAnim.toValue = @(1);
-//    mainImageViewScaleAnim.duration = 0.3f;
-//    [self.mainImageView.layer addAnimation:mainImageViewScaleAnim forKey:@"mainImageView"];
-//    
-//    //subImageView
-//    CABasicAnimation *subImageViewRotationAnim = [CABasicAnimation animationWithKeyPath:@"transform"];
-//    NSValue *fromVal = [NSValue valueWithCATransform3D:CATransform3DMakeRotation(0.f, 0.0f, 0.f, 0.f)];
-//    NSValue *toVal = [NSValue valueWithCATransform3D:CATransform3DMakeRotation(0.5*M_PI, 0.0f, 0.0f, -1.0f)];
-//    subImageViewRotationAnim.fromValue = fromVal;
-//    subImageViewRotationAnim.toValue = toVal;
-//    CABasicAnimation *subImageViewScaleAnim = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
-//    subImageViewScaleAnim.fromValue = @(1);
-//    subImageViewScaleAnim.toValue = @(0);
-//    CAAnimationGroup *subImageViewAnims = [CAAnimationGroup animation];
-//    [subImageViewAnims setAnimations:@[subImageViewRotationAnim, subImageViewScaleAnim]];
-//    subImageViewAnims.duration = 0.3f;
-//    [_subImageView.layer addAnimation:subImageViewAnims forKey:@"subImageView"];
-//    
-//    [CATransaction begin];
-//    [CATransaction setValue:(id)kCFBooleanTrue forKey:kCATransactionDisableActions];
-//    CATransform3D combine;
-//    CATransform3D rotate;
-//    CATransform3D scale;
-//    scale = CATransform3DMakeScale(0.f, 0.f, 0.f);
-//    rotate = CATransform3DMakeRotation(0.5*M_PI, 0.0f, 0.0f, -1.0f);
-//    combine = CATransform3DConcat(rotate, scale);
-//    [_subImageView.layer setTransform:combine];
-//    [CATransaction commit];
+        //subImageView
+        CABasicAnimation *subImageViewRotationAnim = [CABasicAnimation animationWithKeyPath:@"transform"];
+        NSValue *fromVal = [NSValue valueWithCATransform3D:CATransform3DMakeRotation(0.f, 0.0f, 0.f, 0.f)];
+        NSValue *toVal = [NSValue valueWithCATransform3D:CATransform3DMakeRotation(0.5*M_PI, 0.0f, 0.0f, -1.0f)];
+        subImageViewRotationAnim.fromValue = fromVal;
+        subImageViewRotationAnim.toValue = toVal;
+        CABasicAnimation *subImageViewScaleAnim = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+        subImageViewScaleAnim.fromValue = @(1);
+        subImageViewScaleAnim.toValue = @(0);
+        CAAnimationGroup *subImageViewAnims = [CAAnimationGroup animation];
+        [subImageViewAnims setAnimations:@[subImageViewRotationAnim, subImageViewScaleAnim]];
+        subImageViewAnims.duration = 0.3f;
+        [_subImageView.layer addAnimation:subImageViewAnims forKey:@"subImageView"];
+    
+        [CATransaction begin];
+        [CATransaction setValue:(id)kCFBooleanTrue forKey:kCATransactionDisableActions];
+        CATransform3D combine;
+        CATransform3D rotate;
+        CATransform3D scale;
+        scale = CATransform3DMakeScale(0.f, 0.f, 0.f);
+        rotate = CATransform3DMakeRotation(0.5*M_PI, 0.0f, 0.0f, -1.0f);
+        combine = CATransform3DConcat(rotate, scale);
+        [_subImageView.layer setTransform:combine];
+        [CATransaction commit];
+    }
 }
 
 - (void)selectedAnim {
-    //spotImageView
-    _spotImageView.alpha = 1;
-    CABasicAnimation *spotImageViewScale = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
-    spotImageViewScale.fromValue = @(0);
-    spotImageViewScale.toValue = @(1);
-    spotImageViewScale.duration = 0.3f;
-    [_spotImageView.layer addAnimation:spotImageViewScale forKey:@"soptImageView"];
+    if (_itemStatus == HomeTabBarItemStatusWeather) {
+        //spotImageView
+        _spotImageView.alpha = 1;
+        CABasicAnimation *spotImageViewScale = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+        spotImageViewScale.fromValue = @(0);
+        spotImageViewScale.toValue = @(1);
+        spotImageViewScale.duration = 0.3f;
+        [_spotImageView.layer addAnimation:spotImageViewScale forKey:@"soptImageView"];
+        
+        [CATransaction begin];
+        [CATransaction setValue:(id)kCFBooleanTrue forKey:kCATransactionDisableActions];
+        _spotImageView.layer.transform = CATransform3DMakeScale(1, 1, 1);
+        [CATransaction commit];
+    } else if (_itemStatus == HomeTabBarItemStatusReading) {
+        _subImageView.alpha = 0;
+        [UIView animateWithDuration:0.3f animations:^{
+            self.mainImageView.alpha = 0;
+            _subImageView.alpha = 1;
+        } completion:^(BOOL finished) {
+            [self.titleLabel setText:@"刷新"];
+        }];
+        //mainImageView
+        CABasicAnimation *mainImageViewScaleAnim = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+        mainImageViewScaleAnim.fromValue = @(1);
+        mainImageViewScaleAnim.toValue = @(1.2);
+        mainImageViewScaleAnim.duration = 0.3f;
+        [self.mainImageView.layer addAnimation:mainImageViewScaleAnim forKey:@"mainImageView"];
     
-    [CATransaction begin];
-    [CATransaction setValue:(id)kCFBooleanTrue forKey:kCATransactionDisableActions];
-    _spotImageView.layer.transform = CATransform3DMakeScale(1, 1, 1);
-    [CATransaction commit];
-    
-    
-//    _subImageView.alpha = 0;
-//    [UIView animateWithDuration:0.3f animations:^{
-//        self.mainImageView.alpha = 0;
-//        _subImageView.alpha = 1;
-//    } completion:^(BOOL finished) {
-//        [self.titleLabel setText:@"刷新"];
-//    }];
-//    //mainImageView
-//    CABasicAnimation *mainImageViewScaleAnim = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
-//    mainImageViewScaleAnim.fromValue = @(1);
-//    mainImageViewScaleAnim.toValue = @(1.2);
-//    mainImageViewScaleAnim.duration = 0.3f;
-//    [self.mainImageView.layer addAnimation:mainImageViewScaleAnim forKey:@"mainImageView"];
-//    
-//    //subImageView
-//    CABasicAnimation *subImageViewRotationAnim = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
-//    subImageViewRotationAnim.fromValue = @(-0.5*M_PI);
-//    subImageViewRotationAnim.toValue = @(0);
-//    CABasicAnimation *subImageViewScaleAnim = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
-//    subImageViewScaleAnim.fromValue = @(0);
-//    subImageViewScaleAnim.toValue = @(1);
-//    CAAnimationGroup *subImageViewAnims = [CAAnimationGroup animation];
-//    [subImageViewAnims setAnimations:@[subImageViewRotationAnim, subImageViewScaleAnim]];
-//    subImageViewAnims.duration = 0.3f;
-//    [_subImageView.layer addAnimation:subImageViewAnims forKey:@"subImageView"];
-//    //spotImageView
-//    _spotImageView.alpha = 1;
-//    CABasicAnimation *spotImageViewScale = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
-//    spotImageViewScale.fromValue = @(0);
-//    spotImageViewScale.toValue = @(1);
-//    spotImageViewScale.duration = 0.3f;
-//    [_spotImageView.layer addAnimation:spotImageViewScale forKey:@"soptImageView"];
-//    
-//    [CATransaction begin];
-//    [CATransaction setValue:(id)kCFBooleanTrue forKey:kCATransactionDisableActions];
-//    _subImageView.layer.transform = CATransform3DMakeScale(1, 1, 1);
-//    [CATransaction commit];
+        //subImageView
+        CABasicAnimation *subImageViewRotationAnim = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+        subImageViewRotationAnim.fromValue = @(-0.5*M_PI);
+        subImageViewRotationAnim.toValue = @(0);
+        CABasicAnimation *subImageViewScaleAnim = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+        subImageViewScaleAnim.fromValue = @(0);
+        subImageViewScaleAnim.toValue = @(1);
+        CAAnimationGroup *subImageViewAnims = [CAAnimationGroup animation];
+        [subImageViewAnims setAnimations:@[subImageViewRotationAnim, subImageViewScaleAnim]];
+        subImageViewAnims.duration = 0.3f;
+        [_subImageView.layer addAnimation:subImageViewAnims forKey:@"subImageView"];
+        //spotImageView
+        _spotImageView.alpha = 1;
+        CABasicAnimation *spotImageViewScale = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+        spotImageViewScale.fromValue = @(0);
+        spotImageViewScale.toValue = @(1);
+        spotImageViewScale.duration = 0.3f;
+        [_spotImageView.layer addAnimation:spotImageViewScale forKey:@"soptImageView"];
+        
+        [CATransaction begin];
+        [CATransaction setValue:(id)kCFBooleanTrue forKey:kCATransactionDisableActions];
+        _subImageView.layer.transform = CATransform3DMakeScale(1, 1, 1);
+        [CATransaction commit];
+    }
 }
 
 - (void)toReadingStatusAnim {
