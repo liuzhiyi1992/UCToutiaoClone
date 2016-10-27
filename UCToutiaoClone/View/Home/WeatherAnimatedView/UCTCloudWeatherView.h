cccb295edba2a786e121b646bf3f0ca621c430f5
liuzhiyi1992 <55137840@qq.com>
2016-10-27 15:31:16 +0800

    Cloud天气AnimatedView

---
+++ UCToutiaoClone/View/Home/WeatherAnimatedView/UCTCloudWeatherView.h
@@ -0,0 +1,13 @@
+//
+//  UCTCloudWeatherView.h
+//  UCToutiaoClone
+//
+//  Created by zhiyi on 16/10/27.
+//  Copyright © 2016年 lzy. All rights reserved.
+//
+
+#import "UCTWeatherAnimatedView.h"
+
+@interface UCTCloudWeatherView : UCTWeatherAnimatedView
+
+@end
---
+++ UCToutiaoClone/View/Home/WeatherAnimatedView/UCTCloudWeatherView.m
@@ -0,0 +1,78 @@
+//
+//  UCTCloudWeatherView.m
+//  UCToutiaoClone
+//
+//  Created by zhiyi on 16/10/27.
+//  Copyright © 2016年 lzy. All rights reserved.
+//
+
+#import "UCTCloudWeatherView.h"
+#import "Masonry.h"
+#import <QuartzCore/CABase.h>
+#import "MainHomeController.h"
+
+@interface UCTCloudWeatherView ()
+@property (strong, nonatomic) NSArray *imageViewList;
+@end
+
+@implementation UCTCloudWeatherView
+- (void)setupView {
+    NSMutableArray *mutArray = [NSMutableArray array];
+    for (int i = 3; i > 0; i --) {
+        UIImage *image = [UIImage imageNamed:[NSString stringWithFormat:@"Cloud%d", i]];
+        UIImageView *tempImageView = [[UIImageView alloc] initWithImage:image];
+        [self addSubview:tempImageView];
+        [mutArray addObject:tempImageView];
+        
+        [tempImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.centerX.equalTo(self);
+            make.width.equalTo(self).multipliedBy(1.5);
+            make.height.equalTo(tempImageView.mas_width);
+            make.centerY.equalTo(self).offset(-0.25*[[UIScreen mainScreen] bounds].size.width);
+        }];
+    }
+    self.imageViewList = [mutArray copy];
+}
+
+- (void)configureAnim {
+//    UIImageView *firstImageView = _imageViewList.firstObject;
+//    CAKeyframeAnimation *firstAnim = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+//    firstAnim.duration = 7.f;
+//    firstAnim.repeatCount = HUGE_VALF;
+//    UIBezierPath *firstPath = [UIBezierPath bezierPathWithArcCenter:CGPointMake(firstImageView.center.x-15, firstImageView.center.y-3) radius:3 startAngle:0 endAngle:2*M_PI clockwise:YES];
+//    firstAnim.path = firstPath.CGPath;
+//    [firstImageView.layer addAnimation:firstAnim forKey:@"position"];
+//    
+//    UIImageView *secondImageView = _imageViewList[1];
+//    CAKeyframeAnimation *secondAnim = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+//    secondAnim.duration = 6.f;
+//    secondAnim.repeatCount = HUGE_VALF;
+//    UIBezierPath *secondPath = [UIBezierPath bezierPathWithArcCenter:CGPointMake(secondImageView.center.x+5, secondImageView.center.y-5) radius:5 startAngle:0 endAngle:2*M_PI clockwise:YES];
+//    secondAnim.path = secondPath.CGPath;
+//    [secondImageView.layer addAnimation:secondAnim forKey:@"position"];
+//    
+//    UIImageView *thirdImageView = _imageViewList[2];
+//    CAKeyframeAnimation *thirdAnim = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+//    thirdAnim.duration = 5.f;
+//    thirdAnim.repeatCount = HUGE_VALF;
+//    UIBezierPath *thirdPath = [UIBezierPath bezierPathWithArcCenter:CGPointMake(thirdImageView.center.x, thirdImageView.center.y-10) radius:5 startAngle:0 endAngle:2*M_PI clockwise:YES];
+//    thirdAnim.path = thirdPath.CGPath;
+//    [thirdImageView.layer addAnimation:thirdAnim forKey:@"position"];
+}
+
+//- (void)transformWithHomeScrollOffsetY:(CGFloat)offsetY {
+//    offsetY += 20;//除去状态栏
+//    if (offsetY > 0) {
+//        CGFloat scale = 1 - offsetY/150;
+//        self.transform = CGAffineTransformMakeScale(scale, 1);
+//        self.alpha = (CUSTOM_NAV_DISPLAY_HEIGHT-offsetY)/CUSTOM_NAV_DISPLAY_HEIGHT;
+//    }
+//}
+//
+//- (void)transformWithPageScrollOffsetY:(CGFloat)offsetY {
+//    if (offsetY < 0) {
+//        CGFloat scale = 1 + -offsetY/250;
+//        self.transform = CGAffineTransformMakeScale(scale, 1);
+//    }
+//}
+@end
